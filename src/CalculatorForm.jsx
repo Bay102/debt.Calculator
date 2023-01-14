347e86fe68904e0551c2041e4380f251205cbb77
Zak Bay <zacharybay@live.com>
2023-01-14 05:24:15 -0800

    Calculation Complete and Correct

--- src/CalculatorForm.jsx
+++ src/CalculatorForm.jsx
@@ -2,72 +2,81 @@ import React from "react";
 import PaymentHistory from "./PaymentHistory";
 
 class CalculatorForm extends React.Component {
+  state = {
+    balance: "",
+    interest: "",
+    paymentAmount: "",
+  };
 
-   state = {
-      totalStart: '',
-      interest: '',
-      paymentAmount: '',
-      balance: '',
-   }
-      
-   // 1% minimum payment, cannot pay less than min, record every payment and show it in payment history 
-
-   startingBalance = ({target: {value}}) => {this.setState({totalStart: value})}
-
-   paymentAmount = ({target: {value}}) => {this.setState({paymentAmount: value})}
-
-   interestRate = ({target: {value}}) => {this.setState({interest: value})}
-
-   
-   calculations = () => {
-    
-      const loanInterest = (+this.state.interest / 12) * (+this.state.totalStart); 
-      console.log(loanInterest);
-      
-      const principle = (+this.state.totalStart) * 0.01
-      console.log(principle);
-
-      const totalPayment = (+loanInterest) + (+principle);
-      return totalPayment
-   }
+//   settingState = ({target: { value }}, stateValue) => {
+//   }
 
 
-   paymentSubmit = (e) => { 
-      e.preventDefault();
-      this.calculations();
-      const updatedBalance = (+this.totalStart) - (this.calculations()) ; console.log(updatedBalance);
-      this.setState({balance: updatedBalance })
-     
-   }
+  startingBalance = ({ target: { value } }) => {
+    this.setState({ balance: value });
+  };
+  interestRate = ({ target: { value } }) => {
+    this.setState({ interest: value });
+  };
+  paymentAmount = ({ target: { value } }) => {
+    this.setState({ paymentAmount: value });
+  };
 
 
+  calculations = () => {
+    const { balance, interest, paymentAmount } = this.state;
+    const loanInterest = (+interest / 12) * +balance;  console.log(loanInterest);
+    const totalPayment = +paymentAmount - +loanInterest;    
+    this.setState({ balance: balance  -  +totalPayment })
+    return totalPayment;
+  };
 
-   render() {
+  paymentSubmit = (e) => {
+    e.preventDefault();
+    this.calculations();
+  };
 
-      // const {totalStart, interest, paymentAmount, balance} = this.state;
+  render() {
+    const { balance, paymentAmount } = this.state;
 
-      return(
-         <div className="calc-wrapper">
-            <form onSubmit={this.paymentSubmit} action="">
-               <label className="labels" htmlFor="payment-amount">Enter Your Total Loan:</label>
-               <input onChange={this.startingBalance} id="payment-amount" type="number" />
-               <br /><br />
-               <label className="labels" htmlFor="interest-rate">Enter Your Interest Rate:</label>
-               <input onChange={this.interestRate} id="interest-rate" type="number" />
-               <br /><br />
-               <label className="labels" htmlFor="amount">Payment Amount:</label>
-               <input onChange={this.paymentAmount} id="amount" type="number" />
-               <br /><br />
-               <div className="pay-button">
-                  <button onClick={this.paymentSubmit} >Make Payment</button>
-               </div>
-            
-            </form>
-            <hr />
-               <PaymentHistory  balance={this.state.totalStart}/>
-         </div>
-      )
-   }
+    return (
+      <div className="calc-wrapper">
+        <form onSubmit={this.paymentSubmit} action="">
+          <label className="labels" htmlFor="payment-amount">
+            Enter Your Total Loan:
+          </label>
+          <input
+            onChange={this.startingBalance}
+            id="payment-amount"
+            type="number"
+          />
+          <br />
+          <br />
+          <label className="labels" htmlFor="interest-rate">
+            Enter Your Interest Rate:
+          </label>
+          <input
+            onChange={this.interestRate}
+            id="interest-rate"
+            type="number"
+          />
+          <br />
+          <br />
+          <label className="labels" htmlFor="amount">
+            Payment Amount:
+          </label>
+          <input onChange={this.paymentAmount} id="amount" type="number" />
+          <br />
+          <br />
+          <div className="pay-button">
+            <button onClick={this.paymentSubmit}>Make Payment</button>
+          </div>
+        </form>
+        <hr />
+        <PaymentHistory payments={paymentAmount} balance={balance} />
+      </div>
+    );
+  }
 }
 
-export default CalculatorForm; 
+export default CalculatorForm;
--- src/paymentHistory.jsx
+++ src/paymentHistory.jsx
@@ -3,10 +3,16 @@ import React from "react";
 class PaymentHistory extends React.Component {
 
    render() {
-      const { balance } = this.props;
+      const { balance, payments } = this.props;
+      console.log(payments);
       return(
          <div>
             <label htmlFor="current-balance">Current Balance:{balance}</label>
+            <br /><br />
+            <hr />
+            <ul>
+            
+            </ul>
          </div>
       )
    }
